30bbbd955da90e0fc244d3dab746ea312de8acef
ezeabregu <ezeabregu.ea@gmail.com>
2023-11-05 14:29:02 -0300

    data

---
+++ src/components/data/products.js
@@ -0,0 +1,38 @@
+export const products = [
+  {
+    id: 1,
+    title: "Piedra Mar del plata",
+    img: "../../img/products/01.jpeg",
+    size: "52x26",
+  },
+  {
+    id: 2,
+    title: "Travertino",
+    img: "../../img/products/02.jpeg",
+    size: "54x27",
+  },
+  {
+    id: 3,
+    title: "Pizarras",
+    img: "../../img/products/03.jpeg",
+    size: "56x56",
+  },
+  {
+    id: 4,
+    title: "Sol",
+    img: "../../img/products/04.jpeg",
+    size: "60x60",
+  },
+  {
+    id: 5,
+    title: "Frisos(terminaciones)",
+    img: "../../img/products/05.jpeg",
+    size: "c/u 66cm",
+  },
+  {
+    id: 6,
+    title: "Ondas",
+    img: "../../img/products/06.jpeg",
+    size: "60x60",
+  },
+];
