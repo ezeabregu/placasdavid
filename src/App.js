a4d23f0921e5c1e0aa81c09f03ba768de3997aa9
ezeabregu <ezeabregu.ea@gmail.com>
2023-11-08 23:47:40 -0300

    hero changes

--- src/App.js
+++ src/App.js
@@ -6,34 +6,11 @@ import About from "./components/About/About";
 import Products from "./components/Products/Products";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
-import { useEffect } from "react";
 
 function App() {
-  // useEffect(() => {
-  //   const handleScrollButtonVisibility = () => {
-  //     window.pageYOffset > 300 ? setShowButton(true) : setShowButton(false);
-  //   };
-  //   window.addEventListener("scroll", handleScrollButtonVisibility);
-
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScrollButtonVisibility);
-  //   };
-  // }, []);
-
-  // const handleScrollToTop = () => {
-  //   window.scrollTo({ top: 0, behavior: "smooth" });
-  // };
-
   return (
     <>
       <Navbar />
-      {/* {showButton && (
-        <div className="scrollToTop">
-          <button onClick={handleScrollToTop}>
-            <img></img>
-          </button>
-        </div>
-      )} */}
       <Layout>
         <Hero />
         <About />
